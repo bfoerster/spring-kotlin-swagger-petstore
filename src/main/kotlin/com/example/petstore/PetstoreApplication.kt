b25b7031259264a510d048ad2dab9c54766fb2b6
hubuu <foester.bernhard@yahoo.de>
2017-08-04 09:12:06 +0200

    initial setup

---
+++ src/main/kotlin/com/example/petstore/PetstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.example.petstore
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PetstoreApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(PetstoreApplication::class.java, *args)
+}
