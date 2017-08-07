f6ee0894a9678243fc63db692e0fd70cd3b13bdd
Bernhard <f_oobar@yahoo.de>
2017-08-07 16:55:23 +0200

    refacted controller

--- src/main/kotlin/com/example/petstore/api/PetController.kt
+++ src/main/kotlin/com/example/petstore/api/PetController.kt
@@ -1,12 +1,14 @@
 package com.example.petstore.api
 
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
 
 @RestController
+@RequestMapping("/pets")
 class PetController {
 
-    @PostMapping("/pets/{petId}/uploadImage")
+    @PostMapping("/{petId}/uploadImage")
     fun uploadImage(image: File): ApiResponse = ApiResponse(0, "success", "upload successful")
 }
